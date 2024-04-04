dea351533d99505b6fca7d5dee4a0f1d51a9ff23
MykolaStovpiuk <154662251+MykolaStovpiuk@users.noreply.github.com>
2024-04-04 14:41:25 +0300

    2-try

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (totalPrice <= customerCredits) {
-    return `"You ordered ${quantity} droids worth ${totalPrice} credits"`;
-  } else if (totalPrice > customerCredits) {
-    return `"Insufficient funds!"`;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits`;
+  } else {
+    return `Insufficient funds!`;
   }
 }
 
